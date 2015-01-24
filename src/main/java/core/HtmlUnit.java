21fb98639458194bf79b810c96dfb31ece5b8018
SvetlanaBojenkova <bojenkova@outlook.com>
2015-01-23 16:27:00 -0800

    Initial commit

--- src/main/java/core/HtmlUnit.java
+++ src/main/java/core/HtmlUnit.java
@@ -5,13 +5,17 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class HtmlUnit {
-	public static void main(String[] args) {
+	public static void main(String[] l) {
 
 		WebDriver driver = new HtmlUnitDriver();   // Version 1.2 :: HtmlUnit
 
 		String text_case_id = "TC-002.01";
-		String url = "http://www.learn2test.net";
-		String title_expected = "learn2test.net";
+		// String url = "http://www.learn2test.net";
+		// String title_expected = "learn2test.net";
+		String param[] = l[0].split("\\|");
+		
+		String url = param[0];
+		String title_expected = param[1];
 		
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
